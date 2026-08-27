2340a48084fd9f517cee5af42929bbf4193bd902
Appshaker <appshaker@localhost>
2026-08-27 04:06:46 +0200

    feat: add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element #root not found in index.html');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
